b2cc66a0e966b5e068031b4b329abf725fa092e6
Bryan Mullen <bryan@bryanmullen.me>
2022-02-07 20:33:51 +0000

    Define If Extra Box Needed

--- src/io/bryanmullen/Main.java
+++ src/io/bryanmullen/Main.java
@@ -8,5 +8,10 @@ public class Main {
         // Store user input of how many eggs have been laid
         String numberOfEggsLaidStr = JOptionPane.showInputDialog(null, "How many eggs have been laid?");
         int numberOfEggsLaid = Integer.parseInt(numberOfEggsLaidStr);
+
+        // Define whether an extra box might be useful or not
+        int boxSize = 6;
+        boolean extraBoxUseful = numberOfEggsLaid % boxSize == 0;
+
     }
 }
